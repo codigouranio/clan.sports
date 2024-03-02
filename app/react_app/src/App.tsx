8f62604e370145aa807e8b8717fc78fe2a4ce669
Jose Blanco <joseblanco@Joses-MBP.lugo.local>
2024-03-02 01:20:34 -0500

    wip
    Signed-off-by: Jose Blanco <joseblanco@Joses-MBP.lugo.local>

--- app/react_app/src/App.tsx
+++ app/react_app/src/App.tsx
@@ -1,7 +1,44 @@
 import './App.css';
 import logo from './logo.svg';
 import React, { Suspense, lazy } from 'react';
+import {
+  Experimental_CssVarsProvider as CssVarsProvider,
+  useColorScheme,
+} from '@mui/material/styles';
+import Button from '@mui/material/Button';
 // import Dashboard from './Dashboard';
+
+// ModeSwitcher is an example interface for toggling between modes.
+// Material UI does not provide the toggle interface—you have to build it yourself.
+const ModeSwitcher = () => {
+  const { mode, setMode } = useColorScheme();
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    // for server-side rendering
+    // learn more at https://github.com/pacocoursey/next-themes#avoid-hydration-mismatch
+    return null;
+  }
+
+  return (
+    <Button
+      variant="outlined"
+      onClick={() => {
+        if (mode === 'light') {
+          setMode('dark');
+        } else {
+          setMode('light');
+        }
+      }}
+    >
+      {mode === 'light' ? 'Dark' : 'Light'}
+    </Button>
+  );
+};
 
 const NoAccess = lazy(() => import(/* webpackChunkName: "no-access" */ './NoAccess'));
 const Test = lazy(() => import(/* webpackChunkName: "test" */ './Test'));
@@ -11,13 +48,16 @@ const permission = false;
 
 function App() {
   return (
-    <div className="App">
-      <Suspense fallback={<div>Loading...</div>}>
-        {permission &&
-          <Test></Test>
-        }
-        <Dashboard></Dashboard>
-        {/* <header className="App-header">
+    <CssVarsProvider>
+      <ModeSwitcher />
+
+      <div className="App">
+        <Suspense fallback={<div>Loading...</div>}>
+          {permission &&
+            <Test></Test>
+          }
+          <Dashboard></Dashboard>
+          {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
@@ -31,8 +71,10 @@ function App() {
             Learn React
           </a>
         </header> */}
-      </Suspense>
-    </div>
+        </Suspense>
+      </div>
+
+    </CssVarsProvider>
   );
 }
 
