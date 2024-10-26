8ddef3c5f48ab15d758e81fc37c53d52b9725ddf
Código Uranio <codigouranio@gmail.com>
2024-10-26 17:22:44 -0400

    wip

--- alembic/env.py
+++ alembic/env.py
@@ -1,7 +1,6 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 
 from alembic import context
 
@@ -14,11 +13,15 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+# Extract the database URL
+db_url = config.get_main_option("sqlalchemy.url")
+
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from app.api.models import Base    
+from app.api.models import Base
 
 target_metadata = Base.metadata
 
@@ -66,9 +69,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
